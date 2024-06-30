1cf6275aa73824ccc5af49d9c72d5aa829c9dfc6
M-Afan <afansaeed19971719@gmail.com>
2024-06-30 20:37:40 +0500

    bug fixed and reusable components added

--- src/components/EditorOutput.tsx
+++ src/components/EditorOutput.tsx
@@ -1,6 +1,7 @@
 import dynamic from "next/dynamic";
-import Image from "next/image";
 import { FC } from "react";
+import { CustomImageRenderer } from "./renderers/CustomImageRenderer";
+import { CustomCodeRenderer } from "./renderers/CustomCodeRenderer";
 
 const Output = dynamic(
   async () => (await import("editorjs-react-renderer")).default,
@@ -27,7 +28,7 @@ const renderers = {
 
 const EditorOutput: FC<EditorOutputProps> = ({ content }) => {
   return (
-    // @ts-expect-error
+    // @ts-ignore
     <Output
       data={content}
       style={style}
@@ -36,22 +37,5 @@ const EditorOutput: FC<EditorOutputProps> = ({ content }) => {
     />
   );
 };
-
-function CustomImageRenderer({ data }: any) {
-  const src = data.file.url;
-  return (
-    <div className="relative w-full min-h-[15rem]">
-      <Image alt="image" className="object-contain" fill src={src} />
-    </div>
-  );
-}
-
-function CustomCodeRenderer({ data }: any) {
-  return (
-    <pre className="bg-gray-800 rounded-md p-4">
-      <code className="text-gray-100 text-sm">{data.code}</code>
-    </pre>
-  );
-}
 
 export default EditorOutput;
